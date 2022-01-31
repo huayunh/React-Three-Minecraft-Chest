a8364be0e61cb5cab623e6c474f72a65a2794cfa
huayunh <hhuayun0828@gmail.com>
2022-01-31 16:42:53 -0500

    Make chest open

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { Suspense, useState } from 'react';
 //Styles
-import "./assets/styles/App.scss";
+import './assets/styles/App.scss';
+// Three
+import { Canvas, useThree } from 'react-three-fiber';
+import Lights from './components/Three/lights';
+import Floor from './components/Three/floor';
+import { softShadows, Loader, OrbitControls } from '@react-three/drei';
+// Model
+import Model from './components/Three/chest';
+import { useSpring } from '@react-spring/three';
+
+softShadows();
+
+// on load zoom animation
+
+const ZoomWithOrbital = () => {
+    const { gl, camera } = useThree();
+    useSpring({
+        from: {
+            z: 30,
+        },
+        to: {
+            x: -5,
+            y: 4,
+            z: 4,
+        },
+        onFrame: ({ x, y, z }) => {
+            camera.position.x = x;
+            camera.position.y = y;
+            camera.position.y = z;
+        },
+    });
+    return <OrbitControls enableZoom={false} enablePan={false} target={[0, 0, 0]} args={[camera, gl.domElement]} />;
+};
 
 const App = () => {
-  return (
-    <div className='container'>
-      <h3>Hey everyone! Lets learn about React Three Fiber :)</h3>
-      <p>
-        Dont forget to subscribe
-        <a href='https://youtube.com/c/wrongakram'>@wrongakram</a>
-      </p>
-    </div>
-  );
+    const [open, setOpen] = useState(true);
+
+    return (
+        <>
+            <Canvas colorManagement shadowMap camera={{ position: [-5, 4, 4], fov: 40 }}>
+                <Lights />
+                <Suspense fallback={null}>
+                    <Model open={open} setOpen={setOpen} />
+                    <Floor />
+                    <ZoomWithOrbital />
+                </Suspense>
+                <Loader />
+            </Canvas>
+        </>
+    );
 };
 
 export default App;
